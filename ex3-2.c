1c55dc6633dd4f91cf04e4a5996a0ce18d4be752
HowieBenefiel <hben592@gmail.com>
2014-06-11 13:31:44 -0500

    All programs from first chapter.

---
+++ ex3-2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void)
+{
+  printf("In C, lowercase letters are significant.");
+  printf("main is where program execution begins.");
+  printf("Opening and closing brasesenclose program statements in a routine");
